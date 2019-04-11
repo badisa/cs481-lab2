6ae32d7f521dabcaeef5e893fef913de51196c6b
Forrest York <forrest@eyesopen.com>
2019-04-11 12:40:16 -0600

    Fixes minor issues
    * Needed to allocate more memory in efficient case to properly
      compare memory usage
    * Comment was wrong
    * Incorrect formatting in Sprintf

--- cmd/stats_collector.go
+++ cmd/stats_collector.go
@@ -28,6 +28,7 @@ type StatsData struct {
 
 type InputData struct {
 	phase     string
+	toStore   string
 	maxTime   int
 	stepSize  int
 	procs     int
@@ -39,6 +40,7 @@ func main() {
 	phase := flag.String("phases", PHASE_ALL, "Type of phases to run. Options are all, cpu, io, mixed")
 	maxTime := flag.Int("max-time", 5, "Maximum amount of time to run each proc")
 	stepSize := flag.Int("step", 1, "How much to increase time step by until max is reached, also the starting time. In Seconds")
+	toStore := flag.String("input", "deadbeef", "Input to store in memory, either efficiently or not")
 	procs := flag.Int("procs", 10, "How many procs to run in each iteration")
 	batchSize := flag.Int("batch", 10, "Maximum number of processes to run simultaneously")
 	flag.Parse()
@@ -52,6 +54,7 @@ func main() {
 		stepSize:  *stepSize,
 		procs:     *procs,
 		batchSize: *batchSize,
+		toStore:   *toStore,
 	}
 	if *mode == IO_CPU {
 		compareIOCPUProcesses(&inputData)
@@ -90,26 +93,22 @@ func comparePageTableProcesses(input *InputData) {
 	start := time.Now()
 	var wg sync.WaitGroup
 	var key string
-	for runTime := input.stepSize; runTime <= input.maxTime; runTime += input.stepSize {
-		runStart := time.Now()
-		fmt.Printf("Running Mixed Page Table processes for %ds in batches of %d\n", runTime, input.batchSize)
-		key = fmt.Sprintf("mixed-page-table-%d", runTime)
-		for i := 0; i < input.procs; i++ {
-			if i%input.batchSize == 0 {
-				wg.Wait()
-			}
-			wg.Add(1)
-			if i%2 == 0 {
-				go stat.RunInsanePageTableProcess(key, runTime, &wg)
-			} else {
-				go stat.RunSanePageTableProcess(key, runTime, &wg)
-			}
+	fmt.Printf("Running Mixed Page Table processes for input '%s' in batches of %d\n", input.toStore, input.batchSize)
+	key = fmt.Sprintf("mixed-page-table-%s", input.toStore)
+	for i := 0; i < input.procs; i++ {
+		if i%input.batchSize == 0 {
+			wg.Wait()
 		}
-		wg.Wait()
-		fmt.Printf("Finished runs with runTime of %ds, took %s\n", runTime, time.Since(runStart))
+		wg.Add(1)
+		if i%2 == 0 {
+			go stat.RunInsanePageTableProcess(key, input.toStore, &wg)
+		} else {
+			go stat.RunSanePageTableProcess(key, input.toStore, &wg)
+		}
 	}
+	wg.Wait()
 	fmt.Printf("Finished running processes, took %s\n", time.Since(start))
-	path := fmt.Sprintf("lab-part-2-max-%d-step-%d-proc-%d-batches-%d.json", input.maxTime, input.stepSize, input.procs, input.batchSize)
+	path := fmt.Sprintf("lab-part-2-length-%d-step-%d-proc-%d-batches-%d.json", len(input.toStore), input.stepSize, input.procs, input.batchSize)
 	err = stat.Dump(path)
 	if err != nil {
 		fmt.Printf("Failed to dump: %s\n", err)
@@ -218,27 +217,44 @@ func (d *StatsData) WriteRun(key string, data map[string]string) {
 	}
 }
 
-func (d StatsData) RunProcess(path string, time int) (map[string]string, error) {
+func (d StatsData) RunTimedProcess(path string, time int) (map[string]string, error) {
 	schedStats := make(map[string]string, 10)
 	strTime := strconv.Itoa(time)
-	cmd := exec.Command("./insane_page_table", "-time", strTime)
+	cmd := exec.Command(path, "-time", strTime)
 	var output bytes.Buffer
 	cmd.Stdout = &output
 	cmd.Stderr = &output
 	err := cmd.Run()
 	if err != nil {
-		return schedStats, fmt.Errorf("Failed to run insane page table process for time %d: error:%s, output:%s\n", time, err, output.String())
+		return schedStats, fmt.Errorf("Failed to run %s for time %d: error:%s, output:%s\n", path, time, err, output.String())
 	}
 	err = json.Unmarshal(output.Bytes(), &schedStats)
 	if err != nil {
-		return schedStats, fmt.Errorf("Failed to parse IO proc stats: %s\n", output.String())
+		return schedStats, fmt.Errorf("Failed to parse stats:%s, output:%s\n", err, output.String())
 	}
 	return schedStats, nil
 }
 
-func (d *StatsData) RunInsanePageTableProcess(key string, time int, wg *sync.WaitGroup) {
+func (d StatsData) RunInputProcess(path string, input string) (map[string]string, error) {
+	schedStats := make(map[string]string, 10)
+	cmd := exec.Command(path, "-input", input)
+	var output bytes.Buffer
+	cmd.Stdout = &output
+	cmd.Stderr = &output
+	err := cmd.Run()
+	if err != nil {
+		return schedStats, fmt.Errorf("Failed to run %s process for input %s: error:%s, output:%s\n", path, input, err, output.String())
+	}
+	err = json.Unmarshal(output.Bytes(), &schedStats)
+	if err != nil {
+		return schedStats, fmt.Errorf("Failed to parse stats:%s, output:%s\n", err, output.String())
+	}
+	return schedStats, nil
+}
+
+func (d *StatsData) RunInsanePageTableProcess(key, input string, wg *sync.WaitGroup) {
 	defer wg.Done()
-	schedStats, err := d.RunProcess("./insane_page_table", time)
+	schedStats, err := d.RunInputProcess("./insane_page_table", input)
 	if err != nil {
 		fmt.Printf("Failed to run insane page table process for key %s: error:%s\n", key, err)
 		return
@@ -246,9 +262,9 @@ func (d *StatsData) RunInsanePageTableProcess(key string, time int, wg *sync.Wai
 	d.WriteRun(key, schedStats)
 }
 
-func (d *StatsData) RunSanePageTableProcess(key string, time int, wg *sync.WaitGroup) {
+func (d *StatsData) RunSanePageTableProcess(key, input string, wg *sync.WaitGroup) {
 	defer wg.Done()
-	schedStats, err := d.RunProcess("./sane_page_table", time)
+	schedStats, err := d.RunInputProcess("./sane_page_table", input)
 	if err != nil {
 		fmt.Printf("Failed to run sane page table process for key %s: error:%s\n", key, err)
 		return
@@ -258,7 +274,7 @@ func (d *StatsData) RunSanePageTableProcess(key string, time int, wg *sync.WaitG
 
 func (d *StatsData) RunIOProcess(key string, time int, wg *sync.WaitGroup) {
 	defer wg.Done()
-	schedStats, err := d.RunProcess("./io_intensive", time)
+	schedStats, err := d.RunTimedProcess("./io_intensive", time)
 	if err != nil {
 		fmt.Printf("Failed to run IO process for key %s: error:%s\n", key, err)
 		return
@@ -268,7 +284,7 @@ func (d *StatsData) RunIOProcess(key string, time int, wg *sync.WaitGroup) {
 
 func (d *StatsData) RunCPUProcess(key string, time int, wg *sync.WaitGroup) {
 	defer wg.Done()
-	schedStats, err := d.RunProcess("./cpu_intensive", time)
+	schedStats, err := d.RunTimedProcess("./cpu_intensive", time)
 	if err != nil {
 		fmt.Printf("Failed to run CPU process for key %s: error:%s\n", key, err)
 		return
